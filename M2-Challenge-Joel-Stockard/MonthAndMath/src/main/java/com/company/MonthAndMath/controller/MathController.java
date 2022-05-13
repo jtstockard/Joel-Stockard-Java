10e484a5603e89ed8a6f87004946f93c0eb4153f
jtstockard <jtstockard92@gmail.com>
2022-05-12 19:28:36 -0500

    added exception

--- M2-Challenge-Joel-Stockard/MonthAndMath/src/main/java/com/company/MonthAndMath/controller/MathController.java
+++ M2-Challenge-Joel-Stockard/MonthAndMath/src/main/java/com/company/MonthAndMath/controller/MathController.java
@@ -4,16 +4,19 @@ import com.company.MonthAndMath.model.MathSolution;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.Valid;
+
 @RestController
 public class MathController {
 
         @RequestMapping(value="/add", method= RequestMethod.POST)
         @ResponseStatus(value = HttpStatus.CREATED)
-        public MathSolution createAddNumber(@RequestBody MathSolution mathSolution) {
+        public MathSolution createAddNumber(@RequestBody @Valid MathSolution mathSolution) {
             mathSolution.setOperation("add");
             Integer answer = mathSolution.getOperand1() + mathSolution.getOperand2();
             mathSolution.setAnswer(answer);
-            if (mathSolution.getOperand1() != null || mathSolution.getOperand2() != null) {
+            if (mathSolution.getOperand1() == null || mathSolution.getOperand2() == null) {
                 throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "Must be a number");
             } else {
                 return mathSolution;
@@ -22,11 +25,11 @@ public class MathController {
         }//Props to adam and discord friends for helping on this!
         @RequestMapping(value="/subtract", method= RequestMethod.POST)
         @ResponseStatus(value = HttpStatus.CREATED)
-        public MathSolution createSubtractNumber(@RequestBody MathSolution mathSolution) {
+        public MathSolution createSubtractNumber(@RequestBody @Valid MathSolution mathSolution) {
             mathSolution.setOperation("subtract");
             Integer answer = mathSolution.getOperand1() - mathSolution.getOperand2();
             mathSolution.setAnswer(answer);
-            if (mathSolution.getOperand1() != null || mathSolution.getOperand2() != null) {
+            if (mathSolution.getOperand1() == null || mathSolution.getOperand2() == null) {
                 throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "Must be a number");
             } else {
                 return mathSolution;
@@ -35,11 +38,11 @@ public class MathController {
 
         @RequestMapping(value="/multiply", method= RequestMethod.POST)
         @ResponseStatus(value = HttpStatus.CREATED)
-        public MathSolution createMultiplyNumber(@RequestBody MathSolution mathSolution) {
+        public MathSolution createMultiplyNumber(@RequestBody @Valid MathSolution mathSolution) {
             mathSolution.setOperation("multiply");
-            Integer answer = mathSolution.getOperand1() * mathSolution.getOperand2();
+            int answer = mathSolution.getOperand1() * mathSolution.getOperand2();
             mathSolution.setAnswer(answer);
-            if (mathSolution.getOperand1() != null || mathSolution.getOperand2() != null) {
+            if (mathSolution.getOperand1() == null || mathSolution.getOperand2() == null) {
                 throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "Must be a number");
             } else {
                 return mathSolution;
@@ -48,13 +51,13 @@ public class MathController {
 
         @RequestMapping(value="/divide", method= RequestMethod.POST)
         @ResponseStatus(value = HttpStatus.CREATED)
-        public MathSolution createDivideNumber(@RequestBody MathSolution mathSolution) {
+        public MathSolution createDivideNumber(@RequestBody @Valid MathSolution mathSolution) {
             mathSolution.setOperation("divide");
-            Integer answer = mathSolution.getOperand1() / mathSolution.getOperand2();
+            int answer = mathSolution.getOperand1() / mathSolution.getOperand2();
             mathSolution.setAnswer(answer);
             if (mathSolution.getOperand2() == 0) {
                 throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "Cannot divide by 0");
-            } else if (mathSolution.getOperand1() != null || mathSolution.getOperand2() != null) {
+            } else if (mathSolution.getOperand1() == null || mathSolution.getOperand2() == null) {
                 throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "Must be a number");
             } else {
                 return mathSolution;
---
+++ M2-Challenge-Joel-Stockard/MonthAndMath/src/main/java/com/company/MonthAndMath/exception/NotFoundException.java
@@ -0,0 +1,12 @@
+package com.company.MonthAndMath.exception;
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException() {
+        super();
+    }
+
+    public NotFoundException(String msg) {
+        super(msg);
+    }
+}
