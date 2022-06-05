0fa786d4d55095dc42fd538a35713fe2e4374672
Joel Stockard <79311783+jtstockard@users.noreply.github.com>
2022-06-05 10:45:55 -0500

    finish deployment

--- M5-Challenge-Joel-Stockard/gamestore-catalog/src/main/java/com/trilogyed/gamestorecatalog/GamestoreCatalogApplication.java
+++ M5-Challenge-Joel-Stockard/gamestore-catalog/src/main/java/com/trilogyed/gamestorecatalog/GamestoreCatalogApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
 public class GamestoreCatalogApplication {
 
 	public static void main(String[] args) {
---
+++ M5-Challenge-Joel-Stockard/gamestore-invoicing/src/main/resources/static/data.sql
@@ -0,0 +1,62 @@
+use game_store_invoicing;
+
+insert into invoice
+(`name`, street, city, state, zipcode, item_type, item_id, unit_price, quantity, subtotal, tax, processing_fee, total)
+values('Eng',null,'Eagan','MN','55555','Console',1,249.99,1,249.99,10.00,2,261.99);
+
+INSERT INTO tax(state, rate)
+VALUES ('AL',0.05),
+('AK',0.06),
+('AZ',0.04),
+('AR',0.06),
+('CA',0.06),
+('CO',0.04),
+('CT',0.03),
+('DE',0.05),
+('FL',0.06),
+('GA',0.07),
+('HI',0.05),
+('ID',0.03),
+('IL',0.05),
+('IN',0.05),
+('IA',0.04),
+('KS',0.06),
+('KY',0.04),
+('LA',0.05),
+('ME',0.03),
+('MD',0.07),
+('MA',0.05),
+('MI',0.06),
+('MN',0.06),
+('MS',0.05),
+('MO',0.05),
+('MT',0.03),
+('NE',0.04),
+('NV',0.04),
+('NH',0.06),
+('NJ',0.05),
+('NM',0.05),
+('NY',0.06),
+('NC',0.05),
+('ND',0.05),
+('OH',0.04),
+('OK',0.04),
+('OR',0.07),
+('PA',0.06),
+('RI',0.06),
+('SC',0.06),
+('SD',0.06),
+('TN',0.05),
+('TX',0.03),
+('UT',0.04),
+('VT',0.07),
+('VA',0.06),
+('WA',0.05),
+('WV',0.05),
+('WI',0.03),
+('WY',0.04);
+
+INSERT INTO fee(product_type, fee)
+VALUES('console', 14.99),
+('tshirt', 1.98),
+('game',1.49);
